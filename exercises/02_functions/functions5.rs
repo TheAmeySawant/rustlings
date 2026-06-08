bc748b56823085ea0cc39da49108aa114b979e81
Amey Sawant <121055156+TheAmeySawant@users.noreply.github.com>
2026-06-09 01:43:06 +0530

    Completed Exercises:
    - Intro
    - Variables
    - Functions
    - If
    - PrimitiveTypes
    - Vecs
    - MoveSemantics
    - Structs
    - Enums
    - Strings
    - Modules
    - Hashmaps
    - Options

---
+++ exercises/02_functions/functions5.rs
@@ -0,0 +1,9 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i64) -> i64 {
+    num * num
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
---
+++ exercises/07_structs/structs3.rs
@@ -0,0 +1,55 @@
+// Structs contain data, but can also have logic. In this exercise, we have
+// defined the `Fireworks` struct and a couple of functions that work with it.
+// Turn these free-standing functions into methods and associated functions
+// to express that relationship more clearly in the code.
+
+#![deny(clippy::use_self)] // practice using the `Self` type
+
+#[derive(Debug)]
+struct Fireworks {
+    rockets: usize,
+}
+
+impl Fireworks {
+    fn new() -> Self {
+        Self  { rockets: 0 }
+    }
+
+    fn add_rockets(&mut self, rockets: usize) {
+        self.rockets += rockets
+    }
+
+    fn start(self) -> String {
+        "🚀".repeat(self.rockets)
+    }
+}
+// TODO: Turn this function into an associated function on `Fireworks`.
+
+// TODO: Turn this function into a method on `Fireworks`.
+
+// TODO: Turn this function into a method on `Fireworks`.
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn start_some_fireworks() {
+        let f = Fireworks::new();
+        assert_eq!(f.start(), "");
+
+        let mut f = Fireworks::new();
+        f.add_rockets(3);
+        assert_eq!(f.start(), "🚀🚀🚀");
+
+        let mut f = Fireworks::new();
+        f.add_rockets(7);
+        // We don't use method syntax in the last test to ensure the `start`
+        // function takes ownership of the fireworks.
+        assert_eq!(Fireworks::start(f), "🚀🚀🚀🚀🚀🚀🚀");
+    }
+}
